ca3930a333d7305606a160aa63e8aaf84ce6693c
Flochrislas <florent.redares@gmail.com>
2017-11-21 00:06:07 +0900

    Starting RestaurantType
    Reading other tuto for RestaurantType model and table

---
+++ source/app/RestaurantType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RestaurantType extends Model
+{
+    // tuto: https://www.codexworld.com/laravel-tutorial-crud-add-edit-delete-operations/
+    protected $fillable = ['label'];
+}
