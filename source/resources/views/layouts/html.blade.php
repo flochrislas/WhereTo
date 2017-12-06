8e0669264b7a71844672745a5269317e04d57e43
Flochrislas <florent.redares@gmail.com>
2017-12-06 18:53:21 +0900

    Header and Footer in separate blade files
    Header and Footer in separate blade files.

--- source/resources/views/layouts/html.blade.php
+++ source/resources/views/layouts/html.blade.php
@@ -8,44 +8,42 @@
     	<meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <!-- Needed for restaurants CRUD 5.5 tuto -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
-        
+
         <!-- Needed for restaurantType CRUD tuto -->
         <!-- bootstrap minified css -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
         <!-- jQuery library -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <!-- bootstrap minified js -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>        
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <!-- custom CSS -->
         <style>
             h1{font-size:23px;}
             .pull-left h2{margin-top:0;font-size:20px;}
         </style>
-        
+
         <title>{{ config('app.name') }} - @yield('title')</title>
     </head>
     <body>
-    
+
         @section('sidebar')
             @if(config('app.debug') == 1)
         		[Sidebar Section][0]<BR>
                 app.name = {{ config('app.name') }}<BR>
                 app.env = {{ config('app.env') }}<BR>
                 app.debug = {{ config('app.debug') }}<BR>
-				@if(env('POORLY_COPIED') != 'nope')
-					NOT A PROPER COPY OF THE APP!<BR>
-				@endif
-    		@endif        	
+    				@if(env('POORLY_COPIED') != 'nope')
+    					NOT A PROPER COPY OF THE APP!<BR>
+    				@endif
+        		@endif
         @show
 
         <div class="container">
             @yield('content')
         </div>
-        
+
     </body>
 </html>
-
-
