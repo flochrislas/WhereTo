c8e316e9ae6281dbdc8a4a81aed7fe3717265d3a
Flochrislas <florent.redares@gmail.com>
2017-12-14 19:02:26 +0900

    Working n to m
    Working n to m for tags

--- source/database/seeds/RestaurantTagsTableSeeder.php
+++ source/database/seeds/RestaurantTagsTableSeeder.php
@@ -15,107 +15,107 @@ class RestaurantTagsTableSeeder extends Seeder
     {
         DB::table('restaurant_tags')->delete();
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'vegetarian friendly'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'vegan friendly'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'menu with calories'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'menu with allergies'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'customisable dishes'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'italian'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'french'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'mexican'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'indian'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'vietnamese'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'turk'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'indonesian'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'american'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'japanese'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'thai'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'japanese curry'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'sushi'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'izakaya'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'all you can eat'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'all you can drink'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'ramen'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'tonkotsu'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'miso'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'sukiyaki'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'shabushabu'
         ));
 
-        RestaurantTags::create(array(
+        RestaurantTag::create(array(
             'label'              => 'burger'
         ));
 
