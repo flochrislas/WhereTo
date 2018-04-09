ad1b9cddd7bd49467dda337f02cb7736da0bb907
Flochrislas <florent.redares@gmail.com>
2018-04-09 10:51:22 +0900

    Ajax displays actual restaurants list

--- source/resources/views/restaurants/results-data.blade.php
+++ source/resources/views/restaurants/results-data.blade.php
@@ -1,2 +1,36 @@
-<div>AAAA {{ $test }} aaaaya</div>
-TTTT
+<table>
+@foreach ($restaurants as $restaurant)
+  <tr>
+      <td id="nameAndType">
+        <a href="{{ route('restaurants.details',$restaurant->id) }}">
+        <div class="restaurantName">{{ $restaurant->name }}</div>
+        <div class="restaurantType">{{ $restaurant->type }}</div>
+        </a>
+      </td>
+      <td class="center">
+        <a target="_blank"
+          href="{{ $restaurant->google_maps_link }}">
+          {{ App\GeoUtils::walkingTime($restaurant->currentDistance/1000) }}mn
+        </a>
+      </td>
+      <td>
+        <table class="center">
+          <tr>
+            <th>lunch</th>
+            <th>food</th>
+            <th>place</th>
+            <th>price</th>
+            <th>date</th>
+          </tr>
+          <tr>
+            <td>{{ $restaurant->score_lunch }}</td>
+            <td>{{ $restaurant->score_food }}</td>
+            <td>{{ $restaurant->score_place }}</td>
+            <td>{{ $restaurant->score_price }}</td>
+            <td>{{ $restaurant->score_date }}</td>
+          </tr>
+        </table>
+      </td>
+  </tr>
+@endforeach
+</table>
