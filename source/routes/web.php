70168e9463fce76f4a49ce71566df3b540c4c4e9
Flochrislas <florent.redares@gmail.com>
2018-04-16 18:49:16 +0900

    Appending tags to divs
    and data update

--- source/routes/web.php
+++ source/routes/web.php
@@ -33,7 +33,7 @@ Route::get('test-html', array('as' => 'test', function() {
 }));
 
 // Public restaurants pages
-Route::get('restaurants', 'RestaurantController@main')->name('restaurants.main');
+Route::get('restaurants', 'RestaurantTagController@restaurantsFilter')->name('restaurants.main');
 Route::get('restaurants/filter', 'RestaurantController@filterView')->name('restaurants.filter');
 Route::get('restaurants/results', 'RestaurantController@results')->name('restaurants.results');
 Route::get('restaurants/details/{id}', 'RestaurantController@details')->name('restaurants.details');
