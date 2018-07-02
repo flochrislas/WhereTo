85ddbccb01d2efda8ea5c5bbdb0c338c50fb973f
Flochrislas <florent.redares@gmail.com>
2018-07-02 19:05:22 +0900

    Progress on style for restaurants
    Details, but not only. And data update.

--- source/resources/views/restaurants/details.blade.php
+++ source/resources/views/restaurants/details.blade.php
@@ -26,18 +26,18 @@
 
   <div class="detailRow">
       <div class="detail">{{ $restaurant->lunch_price}}&yen;</div>
-      <div class="detail">Lunch {{ $restaurant->score_lunch}}/4</div>
-      <div class="detail">Food {{ $restaurant->score_food}}/3</div>
-      <div class="detail">Place{{ $restaurant->score_place}}/3</div>
-      <div class="detail">Cost {{ $restaurant->score_price}}/2</div>
-      <div class="detail">Date {{ $restaurant->score_date}}/2</div>
+      <div class="detail"><p>Lunch</p>{{ $restaurant->score_lunch}}/4</div>
+      <div class="detail"><p>Food</p>{{ $restaurant->score_food}}/3</div>
+      <div class="detail"><p>Place</p>{{ $restaurant->score_place}}/3</div>
+      <div class="detail"><p>Cost</p>{{ $restaurant->score_price}}/2</div>
+      <div class="detail"><p>Date</p>{{ $restaurant->score_date}}/2</div>
   </div>
 
   <div class="detailRow">
       <div class="detail">{{ $restaurant->points}}</div>
   </div>
 
-  <div class="detailRow">
+  <div class="detailRow justified">
       <div class="detail">{{ $restaurant->experience}}</div>
   </div>
 
@@ -51,7 +51,7 @@
       </div>
   @endforeach
   </div>
-<div class="mysteryDot"> . </div>
+
   @if ($restaurant->visited != 1)
     <div class="mysteryDot"> . </div>
   @endif
