b2669de3f0dd9d833b6ed25d9c07bd2f8eb81eb9
Flochrislas <florent.redares@gmail.com>
2018-07-27 16:55:40 +0900

    CSS for home page grid
    flex grid on home page

--- source/resources/views/home.blade.php
+++ source/resources/views/home.blade.php
@@ -7,8 +7,33 @@
           {{ session('status') }}
       </div>
   @endif
+  <div class="grid">
+    <a class="giantButton" href="{{ route('restaurants.main') }}">
+      Lunch
+      <br/>in<br/>Shibuya</a>
 
-  <a class="giantButton" href="{{ route('restaurants.main') }}">Lunch in<br/>Shibuya</a>
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      Shopping
+      <br/>in<br/>Shibuya</a>
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      Dating
+      <br/>in<br/>Shibuya</a>
 
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      WhatsNext<br/>Extension<br/>(locked)</a>
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      Hashigo<br/>Extension<br/>(locked)</a>
+
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      Clubbing in<br/>Shibuya</a>
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      Drinking in<br/>Shibuya</a>
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      Karaoke
+      <br/>in<br/>Shibuya</a>
+    <a class="giantButton locked" href="{{ route('restaurants.main') }}">
+      Hair Salon
+      <br/>in<br/>Shibuya</a>
+  </div>
 </div>
 @endsection
