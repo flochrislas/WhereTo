30608b6129b0520ae5344a4f4a11403b310fb140
Flochrislas <florent.redares@gmail.com>
2018-08-01 19:04:16 +0900

    Details page colors
    and data update

--- source/resources/views/home.blade.php
+++ source/resources/views/home.blade.php
@@ -15,46 +15,46 @@
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Shopping
       <br/>in<br/>Shibuya
-      <div class="price">50</div>
+      <div class="purchasePrice">50</div>
     </a>
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Dating
       <br/>in<br/>Shibuya
-      <div class="price">50</div>
+      <div class="purchasePrice">50</div>
     </a>
 
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       WhatsNext<br/>Extension
-      <div class="price">120</div>
+      <div class="purchasePrice">120</div>
     </a>
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Hashigo<br/>Extension<br/>
-      <div class="price">120</div>
+      <div class="purchasePrice">120</div>
     </a>
 
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Clubbing in<br/>Shibuya
-      <div class="price">50</div>
+      <div class="purchasePrice">50</div>
     </a>
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Drinking in<br/>Shibuya
-      <div class="price">50</div>
+      <div class="purchasePrice">50</div>
     </a>
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Karaoke
       <br/>in<br/>Shibuya
-      <div class="price">50</div>
+      <div class="purchasePrice">50</div>
     </a>
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Hair Salon
       <br/>in<br/>Shibuya
-      <div class="price">50</div>
+      <div class="purchasePrice">50</div>
     </a>
 
     <a class="giantButton locked" href="{{ route('restaurants.main') }}">
       Shibuya's Secrets
       <br/>MEGAPACK
-      <div class="price">500</div>
+      <div class="purchasePrice">500</div>
     </a>
   </div>
 </div>
