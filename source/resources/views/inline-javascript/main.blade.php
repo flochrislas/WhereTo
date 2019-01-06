0bda360f178f3f6c3840ed701acde5027923ce76
Flochrislas <florent.redares@gmail.com>
2019-01-07 01:10:19 +0900

    fix geolocalisation and remove vip and info from UI
    fix geolocalisation and remove vip and info from UI

--- source/resources/views/inline-javascript/main.blade.php
+++ source/resources/views/inline-javascript/main.blade.php
@@ -21,6 +21,10 @@ function getResults() {
   // Get all if no places loaded
   if (isResultEmpty()) {
     var xhr = new XMLHttpRequest();
+    var x = document.getElementById("position-report");
+    var url = resultsUrl
+            + '?position=' + x.value;
+    console.info('calling: '+url),
     xhr.open('GET', resultsUrl);
     xhr.onload = function() {
         if (xhr.status === 200) {
@@ -41,29 +45,47 @@ function isResultEmpty() {
 <!-- Javascript for geolocalisation -->
 <script>
 var x = document.getElementById("position-report");
-var y = document.getElementById("position");
+// var y = document.getElementById("position");
 getLocation();
+
+/** Options for getCurrentPosition. Time in ms */
+var getCurrentPositionOptions = {
+  enableHighAccuracy: true,
+  timeout: 5500,
+  maximumAge: 1500
+};
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        // This needs valid HTTPS age, or localhost, to work in Chrome!!!
+        // And beware the timeout or delay!!!
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, getCurrentPositionOptions);
         // it is also possible to get continuously updated coords from the method watchPosition()
         // see https://www.w3schools.com/html/html5_geolocation.asp
+        // Console test:
+        // navigator.geolocation.getCurrentPosition(function(position){console.info(position.coords.latitude+', '+position.coords.longitude)},function(error){console.warn(error.code+': '+error.message)});
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
+        console.warn("Geolocation is not supported by this browser.");
     }
 }
 
-function showPosition(position) {
+/** getCurrentPosition sucess */
+function onSuccess(position) {
     /*
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
     */
-    // Was y !!
+    // TODO: use session store ?
     x.value = position.coords.latitude + "," + position.coords.longitude;
+    // For debugging purposes:
+    // console.info('Geolocalisation successfull: '+ position.coords.latitude +', '+ position.coords.longitude);
+    console.info('Geolocalisation: '+ x.value);
 }
 
-function showError(error) {
+/** getCurrentPosition failure */
+function onError(error) {
+    console.warn('Geolocalisation Failure (${error.code}): ${error.message}');
     switch(error.code) {
         case error.PERMISSION_DENIED:
             x.innerHTML = "User denied the request for Geolocation."
@@ -77,6 +99,12 @@ function showError(error) {
         case error.UNKNOWN_ERROR:
             x.innerHTML = "An unknown error occurred."
             break;
+    }
+    if (error.message.startsWith("Only secure")) {
+      x.innerHTML = " Please use the HTTPS version of the service to enable geolocalisation."
+    }
+    else {
+      x.innerHTML = " ERREUR (${error.code}): ${error.message}"
     }
 }
 </script>
--- source/resources/views/layouts/footer.blade.php
+++ source/resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <nav id="footer">
   <div class="copyright">
-      copyright 2018
+      copyright 2017-2019
   </div>
 </nav>
