51ba397036b84457f78245e78103412238274b14
Flochrislas <florent.redares@gmail.com>
2019-06-21 19:28:44 +0900

    small addition data

--- source/database/seeds/RestaurantsTableSeeder.php
+++ source/database/seeds/RestaurantsTableSeeder.php
@@ -4912,6 +4912,9 @@ $resto = Restaurant::create(array(
         // black brown next to labi
         // baachan B1F past arestrant
         // 650 -> dai 830
+
+        // Ottotto
+        // Nice and clean, the bar offers various dishes of pasta, a burger, and something different depending on the day. For less than 1000 yen, you can get a dish, in regular or big quantity, a salad with soup, and free access to water and coffee (hot and cold). I tried spicy bolognese spaghetti, it was quite spicy, good. Amount was good too. Service was good, the place felt nice, and made for a pleasant experience. It is non smoking but they have individual smoking booths you can use inside. The front is opened to the outside when the weather allows it.
  
       
 /*
