b1ea0be982847f176557d0d5e355a45b20f420e8
Flochrislas <florent.redares@gmail.com>
2019-08-15 12:28:38 +0900

    Fix ellipsis in description list
    no escape html there

--- source/resources/views/karaokes/results-data.blade.php
+++ source/resources/views/karaokes/results-data.blade.php
@@ -18,7 +18,7 @@
       </a>
     </div>
     <div class="resultCell description">
-        <div class="extract">{{ $karaoke->points }}</div>
+        <div class="extract">{!! $karaoke->points !!}</div>
     </div>
   </div>
 @endforeach
